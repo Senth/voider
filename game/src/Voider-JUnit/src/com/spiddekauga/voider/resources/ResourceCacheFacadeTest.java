5018cc0ef36b94f3dd68a606ed480d41131bca16
senth <senth@cerina>
2012-12-13 14:24:44 +0100

    Refs #23 (0.1) Testing to commit time.

---
+++ game/src/Voider-JUnit/src/com/spiddekauga/voider/resources/ResourceCacheFacadeTest.java
@@ -0,0 +1,62 @@
+package com.spiddekauga.voider.resources;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * 
+ * 
+ * @author Matteus Magnusson <senth.wallace@gmail.com>
+ */
+public class ResourceCacheFacadeTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.spiddekauga.voider.resources.ResourceCacheFacade#loadAllOf(java.lang.Class, boolean)}.
+	 */
+	@Test
+	public void testLoadAllOf() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.spiddekauga.voider.resources.ResourceCacheFacade#load(java.util.UUID, java.lang.Class, com.spiddekauga.voider.resources.Def)}.
+	 */
+	@Test
+	public void testLoadUUIDClassOfResourceTypeDef() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.spiddekauga.voider.resources.ResourceCacheFacade#load(com.spiddekauga.voider.resources.Def, boolean)}.
+	 */
+	@Test
+	public void testLoadDefBoolean() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.spiddekauga.voider.resources.ResourceCacheFacade#load(com.spiddekauga.voider.resources.ResourceNames)}.
+	 */
+	@Test
+	public void testLoadResourceNames() {
+		fail("Not yet implemented");
+	}
+
+}
