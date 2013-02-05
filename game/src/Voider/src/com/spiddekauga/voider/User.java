a7e7661ed997b73fdaf46ff7d451e75fcf4a4b01
senth <senth.wallace@gmail.com>
2013-02-05 08:12:59 +0100

    Fixed #89 (0.2) Implemented stub uer.

---
+++ game/src/Voider/src/com/spiddekauga/voider/User.java
@@ -0,0 +1,29 @@
+package com.spiddekauga.voider;
+
+/**
+ * User class container
+ * 
+ * @author Matteus Magnusson <senth.wallace@gmail.com>
+ */
+public class User {
+	/**
+	 * @return nickname of the user
+	 */
+	public static String getNickName() {
+		return "Senth";
+	}
+
+	/**
+	 * @return id of the user
+	 */
+	public static int getId() {
+		return 0;
+	}
+
+	/**
+	 * @return email of the user
+	 */
+	public static String getEmail() {
+		return "senth.wallace@gmail.com";
+	}
+}
