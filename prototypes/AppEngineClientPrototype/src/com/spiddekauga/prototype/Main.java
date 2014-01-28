0202d0f14b9fec9a4f88b938adaf80479b9784f0
senth <senth.wallace@gmail.com>
2014-01-28 12:53:00 +0100

    Refs #72 (1.25) Updated initial projects.

---
+++ prototypes/AppEngineClientPrototype/src/com/spiddekauga/prototype/Main.java
@@ -0,0 +1,16 @@
+package com.spiddekauga.prototype;
+
+/**
+ * 
+ * 
+ * @author Matteus Magnusson <senth.wallace@gmail.com>
+ */
+public class Main {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String args[]) {
+
+	}
+}
---
+++ prototypes/AppEngineServletPrototype/src/com/spiddekauga/prototype/AppEngineServletPrototypeServlet.java
@@ -0,0 +1,12 @@
+package com.spiddekauga.prototype;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class AppEngineServletPrototypeServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
