89b8c06624a52adc569245fa8eaa15282e3b2cd9
senth <senth.wallace@gmail.com>
2014-01-28 17:47:33 +0100

    Refs #72 (2.15) Prototype client can now talk with the server using a very simple get method.

--- prototypes/AppEngineClientPrototype/src/com/spiddekauga/prototype/Main.java
+++ prototypes/AppEngineClientPrototype/src/com/spiddekauga/prototype/Main.java
@@ -1,5 +1,21 @@
 package com.spiddekauga.prototype;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+
+
+
 /**
  * 
  * 
@@ -7,10 +23,66 @@ package com.spiddekauga.prototype;
  */
 public class Main {
 	/**
-	 * 
 	 * @param args
 	 */
 	public static void main(String args[]) {
+		createNewUserGet();
+	}
+
+	/**
+	 * Tries to create a new user
+	 */
+	public static void createNewUserGet() {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+
+		try {
+			URI uri = new URIBuilder()
+			.setScheme("http")
+			.setHost(mServer)
+			.setPath("/newuser")
+			.setParameter("username", "senth")
+			.setParameter("password", "bajs")
+			.build();
+
+			HttpGet httpGet = new HttpGet(uri);
+
+			CloseableHttpResponse response = null;
+			try {
+				response = httpClient.execute(httpGet);
+				HttpEntity entity = response.getEntity();
+
+				if (entity != null) {
+					InputStream contentStream = entity.getContent();
+					try {
+						long len = entity.getContentLength();
+						if (len != -1 && len < CONTENT_STRING_LENGTH_MAX) {
+							System.out.println(EntityUtils.toString(entity));
+						}
+					} finally {
+						contentStream.close();
+					}
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				if (response != null) {
+					try {
+						response.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
 
 	}
+
+	/** Maximum content string length */
+	private final static long CONTENT_STRING_LENGTH_MAX = 2048;
+	/** Web app location */
+	private static String mServer = "localhost:8888";
 }
---
+++ prototypes/AppEngineServletPrototype/src/com/spiddekauga/prototype/NewUser.java
@@ -0,0 +1,28 @@
+package com.spiddekauga.prototype;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Creates a new user
+ * 
+ * @author Matteus Magnusson <senth.wallace@gmail.com>
+ */
+@SuppressWarnings("serial")
+public class NewUser extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Set response type
+		response.setContentType("text/plain");
+
+		PrintWriter out = response.getWriter();
+
+		out.println("You user is: " + request.getParameter("username") + "\n"
+				+ "Your password is: " + request.getParameter("password"));
+	}
+}
