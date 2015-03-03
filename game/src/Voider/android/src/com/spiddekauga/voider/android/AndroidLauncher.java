8222fb84f883c282a66914119337b20e30be5f61
senth <senth.wallace@gmail.com>
2015-03-03 16:45:08 +0100

    Migration to gradle.

---
+++ game/src/Voider/android/src/com/spiddekauga/voider/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.spiddekauga.voider.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.spiddekauga.voider.VoiderGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new VoiderGame(), config);
+	}
+}
