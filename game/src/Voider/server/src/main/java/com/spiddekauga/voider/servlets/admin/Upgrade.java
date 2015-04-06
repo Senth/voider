76e6b5dbc6065c7241aff4f185597e57f44597c8
senth <senth.wallace@gmail.com>
2015-04-06 15:56:23 +0200

    Refs #535 (1.35)  * Fixed duplicate published levels in beta.

--- game/src/Voider/server/src/main/java/com/spiddekauga/voider/servlets/admin/Upgrade.java
+++ game/src/Voider/server/src/main/java/com/spiddekauga/voider/servlets/admin/Upgrade.java
@@ -7,16 +7,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.Random;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 
 import net._01001111.text.LoremIpsum;
 
+import com.google.appengine.api.blobstore.BlobKey;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.search.Document;
 import com.google.appengine.api.search.Field;
+import com.spiddekauga.appengine.BlobUtils;
 import com.spiddekauga.appengine.DatastoreUtils;
 import com.spiddekauga.appengine.SearchUtils;
 import com.spiddekauga.voider.network.entities.IEntity;
@@ -30,6 +33,8 @@ import com.spiddekauga.voider.server.util.ServerConfig.DatastoreTables.CAnalytic
 import com.spiddekauga.voider.server.util.ServerConfig.DatastoreTables.CMotd;
 import com.spiddekauga.voider.server.util.ServerConfig.DatastoreTables.CPublished;
 import com.spiddekauga.voider.server.util.ServerConfig.DatastoreTables.CResourceComment;
+import com.spiddekauga.voider.server.util.ServerConfig.DatastoreTables.CSyncPublished;
+import com.spiddekauga.voider.server.util.ServerConfig.SearchTables;
 import com.spiddekauga.voider.server.util.ServerConfig.TokenSizes;
 import com.spiddekauga.voider.server.util.UserRepo;
 import com.spiddekauga.voider.server.util.VoiderServlet;
@@ -47,7 +52,7 @@ public class Upgrade extends VoiderServlet {
 
 	@Override
 	protected IEntity onRequest(IMethodEntity methodEntity) throws ServletException, IOException {
-
+		// removeDuplicateResources();
 
 		getResponse().setContentType("text/html");
 		getResponse().getWriter().append("DONE !");
@@ -55,8 +60,138 @@ public class Upgrade extends VoiderServlet {
 		return null;
 	}
 
-	private void removeDuplicateLevels() {
+	private void removeDuplicateResources() {
+		// Get duplicates
+		Iterable<Entity> resources = DatastoreUtils.getEntities(DatastoreTables.PUBLISHED);
+		HashMap<UUID, Entity> duplicateCheck = new HashMap<>();
+		ArrayList<Duplicate> duplicates = new ArrayList<>();
+		for (Entity entity : resources) {
+			UUID resourceId = DatastoreUtils.getPropertyUuid(entity, CPublished.RESOURCE_ID);
+			if (resourceId != null) {
+				if (duplicateCheck.containsKey(resourceId)) {
+					Duplicate duplicate = new Duplicate();
+					duplicate.a = entity;
+					duplicate.b = duplicateCheck.get(resourceId);
+					duplicates.add(duplicate);
+				} else {
+					duplicateCheck.put(resourceId, entity);
+				}
+			}
+		}
 
+		// Check which one of the duplicates we should remove. The one we shouldn't remove
+		// exists in search
+		for (Duplicate duplicate : duplicates) {
+			UploadTypes type = DatastoreUtils.getPropertyIdStore(duplicate.a, CPublished.TYPE, UploadTypes.class);
+			String tableName = "";
+			switch (type) {
+			case BULLET_DEF:
+				tableName = SearchTables.BULLET;
+				break;
+			case ENEMY_DEF:
+				tableName = SearchTables.ENEMY;
+				break;
+			case LEVEL_DEF:
+				tableName = SearchTables.LEVEL;
+				break;
+			default:
+				break;
+			}
+
+			Document documentA = SearchUtils.getDocument(tableName, KeyFactory.keyToString(duplicate.a.getKey()));
+			Document documentB = SearchUtils.getDocument(tableName, KeyFactory.keyToString(duplicate.b.getKey()));
+
+			// Remove B
+			if (documentA != null && documentB == null) {
+				duplicate.toRemove = duplicate.b;
+			}
+			// Remove A
+			else if (documentB != null && documentA == null) {
+				duplicate.toRemove = duplicate.a;
+			}
+			// Error none was found
+			else if (documentA == null && documentB == null) {
+				mLogger.severe("No document was found when removing duplicate resources");
+			}
+			// Both was found
+			else if (documentA != null && documentB != null) {
+				mLogger.severe("Document was found for both resources");
+			}
+		}
+
+
+		// Get all sync_published
+		Iterable<Entity> syncPublished = DatastoreUtils.getEntities(DatastoreTables.SYNC_PUBLISHED);
+		HashMap<Key, ArrayList<Key>> syncKeys = new HashMap<>();
+		for (Entity entity : syncPublished) {
+			Key publishKey = (Key) entity.getProperty(CSyncPublished.PUBLISHED_KEY);
+
+			ArrayList<Key> keys = syncKeys.get(publishKey);
+			if (keys == null) {
+				keys = new ArrayList<>();
+				syncKeys.put(publishKey, keys);
+			}
+			keys.add(entity.getKey());
+		}
+
+
+		// Remove duplicates
+		ArrayList<Key> entitiesToRemove = new ArrayList<>();
+		for (Duplicate duplicate : duplicates) {
+			if (duplicate.toRemove == null) {
+				continue;
+			}
+
+			Key publishedKey = duplicate.toRemove.getKey();
+			entitiesToRemove.add(publishedKey);
+
+			UploadTypes type = DatastoreUtils.getPropertyIdStore(duplicate.toRemove, CPublished.TYPE, UploadTypes.class);
+
+			// Remove blob
+			BlobKey blobKey = (BlobKey) duplicate.toRemove.getProperty(CPublished.BLOB_KEY);
+			if (blobKey != null) {
+				BlobUtils.deleteBlob(blobKey);
+			}
+
+			// Remove sync_published
+			ArrayList<Key> keysToRemove = syncKeys.get(publishedKey);
+			if (keysToRemove != null) {
+				entitiesToRemove.addAll(keysToRemove);
+			}
+
+			// Remove dependencies of this resource
+			List<Key> dependencyKeys = DatastoreUtils.getKeys(DatastoreTables.DEPENDENCY, publishedKey);
+			if (dependencyKeys != null) {
+				entitiesToRemove.addAll(dependencyKeys);
+			}
+
+			// Remove level specific things
+			if (type == UploadTypes.LEVEL_DEF) {
+				// Remove stats
+				Key statKey = DatastoreUtils.getSingleKey(DatastoreTables.LEVEL_STAT, publishedKey);
+				if (statKey != null) {
+					entitiesToRemove.add(statKey);
+				}
+
+				// Remove tags
+				Key tagKey = DatastoreUtils.getSingleKey(DatastoreTables.LEVEL_TAG, publishedKey);
+				if (tagKey != null) {
+					entitiesToRemove.add(tagKey);
+				}
+			}
+
+		}
+
+		// Actually remove from the datastore
+		mLogger.info("Deleting " + entitiesToRemove.size() + " entities");
+		mLogger.info("Deleting: " + entitiesToRemove);
+		DatastoreUtils.delete(entitiesToRemove);
+	}
+
+	private class Duplicate {
+		private Entity a = null;
+		private Entity b = null;
+		private Entity toRemove = null;
 	}
 
 	private void createMotd() {
