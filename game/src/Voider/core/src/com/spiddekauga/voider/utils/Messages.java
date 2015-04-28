b034e88137318d636853421ced6a676a92fe5df4
senth <senth.wallace@gmail.com>
2015-04-28 13:20:32 +0200

    Refs #335  * Finished setting captions on YouTube.  * Started changing tooltips

--- game/src/Voider/core/src/com/spiddekauga/voider/utils/Messages.java
+++ game/src/Voider/core/src/com/spiddekauga/voider/utils/Messages.java
@@ -217,52 +217,52 @@ public class Messages {
 	@SuppressWarnings("javadoc")
 	public enum EditorTooltips implements ITooltip {
 		// Level tabs
-		TAB_ENEMY_ADD("Add enemy tab"),
+		TAB_ENEMY_ADD("Add enemy tab", "07m21s"),
 		TAB_ENEMY("Enemy settings"),
-		TAB_PATH("Path settings"),
-		TAB_COLOR_LEVEL("Terrain color"),
+		TAB_PATH("Path settings", "09m05s"),
+		TAB_COLOR_LEVEL("Terrain color", "05m13s"),
 
 		// Actor tabs
-		TAB_VISUAL("Visual settings", null, 1),
-		TAB_COLOR_ACTOR("Color settings", null, 1),
+		TAB_VISUAL("Shape settings", "18m53s", 1),
+		TAB_COLOR_ACTOR("Color settings", "21m27s", 1),
 
 		// Enemy tabs
-		TAB_MOVEMENT("Movement settings", null, 1),
-		TAB_WEAPON("Weapon settings", "https://www.youtube.com/watch?v=SxzoVL5YTHc", 1),
-		TAB_COLLISION("Collision settings", "https://www.youtube.com/watch?v=ZbKIrmIbrjk", 1),
+		TAB_MOVEMENT("Movement settings", "13m16s", 1),
+		TAB_WEAPON("Weapon settings", "15m35s", 1),
+		TAB_COLLISION("Collision settings", "21m43s", 1),
 
 		// Bullet tabs
-		TAB_BULLET_TEST("Test bullet on weapons", null, 1),
+		TAB_BULLET_TEST("Test bullet on weapons", "22m35s", 1),
 
 		// Tools
-		TOOL_SELECTION("Selection tool (S)", "https://www.youtube.com/watch?v=7R1rK2b8jaU", 1),
-		TOOL_PAN_LEVEL("Pan tool", "https://www.youtube.com/watch?v=WThGYvHQSHU", 1),
+		TOOL_SELECTION("Selection tool (S)", , 1),
+		TOOL_PAN_LEVEL("Pan tool", , 1),
 		TOOL_PAN_ACTOR(TOOL_PAN_LEVEL.mText, TOOL_PAN_LEVEL.mYoutubeLink, TAB_VISUAL, 2),
-		TOOL_MOVE_LEVEL("Move tool (M)", "https://www.youtube.com/watch?v=UFmqy6YWMRk", 1),
+		TOOL_MOVE_LEVEL("Move tool (M)", , 1),
 		TOOL_MOVE_ACTOR(TOOL_MOVE_LEVEL.mText, TOOL_MOVE_LEVEL.mYoutubeLink, TAB_VISUAL, 2),
-		TOOL_DELETE_LEVEL("Delete tool (Del)", "https://www.youtube.com/watch?v=96S2M17STaI", 1),
+		TOOL_DELETE_LEVEL("Delete tool (Del)", , 1),
 		TOOL_DELETE_ACTOR(TOOL_DELETE_LEVEL.mText, TOOL_DELETE_LEVEL.mYoutubeLink, TAB_VISUAL, 2),
-		TOOL_CLEAR_SELECTION("Clear selection (Esc/Back)"),
-		TOOL_ZOOM_IN_LEVEL("Zoom in", "http://www.youtube.com/watch?v=WThGYvHQSHU", 1),
-		TOOL_ZOOM_OUT_LEVEL("Zoom out", "http://www.youtube.com/watch?v=WThGYvHQSHU", 1),
-		TOOL_ZOOM_RESET_LEVEL("Reset zoom", "http://www.youtube.com/watch?v=WThGYvHQSHU", 1),
+		TOOL_CLEAR_SELECTION("Clear selection (Esc/Back)", ),
+		TOOL_ZOOM_IN_LEVEL("Zoom in", , 1),
+		TOOL_ZOOM_OUT_LEVEL("Zoom out", , 1),
+		TOOL_ZOOM_RESET_LEVEL("Reset zoom", , 1),
 		TOOL_ZOOM_IN_ACTOR(TOOL_ZOOM_IN_LEVEL.mText, TOOL_ZOOM_IN_LEVEL.mYoutubeLink, TAB_VISUAL, 2),
 		TOOL_ZOOM_OUT_ACTOR(TOOL_ZOOM_OUT_LEVEL.mText, TOOL_ZOOM_OUT_LEVEL.mYoutubeLink, TAB_VISUAL, 2),
 		TOOL_ZOOM_RESET_ACTOR(TOOL_ZOOM_RESET_LEVEL.mText, TOOL_ZOOM_RESET_LEVEL.mYoutubeLink, TAB_VISUAL, 2),
-		TOOL_DRAW_APPEND_TERRAIN("Draw terrain (D)", "https://www.youtube.com/watch?v=UbOF_KgpLzI", 1),
-		TOOL_DRAW_ERASE_TERRAIN("Add/Remove area to/from terrain (Shift+D)", "https://www.youtube.com/watch?v=UbOF_KgpLzI", 1),
-		TOOL_DRAW_CORNER_ADD_TERRAIN("Add/Move terrain corners", "https://www.youtube.com/watch?v=UbOF_KgpLzI", 1),
-		TOOL_DRAW_CORNER_REMOVE_TERRAIN("Remove terrain corners", "https://www.youtube.com/watch?v=UbOF_KgpLzI", 1),
-		TOOL_DRAW_APPEND_ACTOR("Draw shape (T)", "https://www.youtube.com/watch?v=q9BmR6E5JvM", TAB_VISUAL, 2),
-		TOOL_DRAW_ERASE_ACTOR("Add/Remove area to/from shape (Shift+T)", "https://www.youtube.com/watch?v=q9BmR6E5JvM", TAB_VISUAL, 2),
-		TOOL_DRAW_CORNER_ADD_ACTOR("Add/Move shape corners (C)", "https://www.youtube.com/watch?v=q9BmR6E5JvM", TAB_VISUAL, 2),
-		TOOL_DRAW_CORNER_REMOVE_ACTOR("Remove shape corners (Shift+C)", "https://www.youtube.com/watch?v=q9BmR6E5JvM", TAB_VISUAL, 2),
-		TOOL_ENEMY_ADD("Add enemies (E)", "https://www.youtube.com/watch?v=n3d31c4gaf8", 1),
-		TOOL_PATH("Add paths for enemies (P)", "https://www.youtube.com/watch?v=DWeUECW8o2w", 1),
-		TOOL_TRIGGER_ACTIVATE("Set enemy activate triggers (T)", "https://www.youtube.com/watch?v=eCTuQdG3v98", 1),
-		TOOL_TRIGGER_DEACTIVATE("Set enemy deactivate triggers (Shift+T)", "https://www.youtube.com/watch?v=eCTuQdG3v98", 1),
-		TOOL_CENTER_SET("Set center", "https://www.youtube.com/watch?v=q9BmR6E5JvM", TAB_VISUAL, 2),
-		TOOL_CENTER_RESET("Reset center", "https://www.youtube.com/watch?v=q9BmR6E5JvM", TAB_VISUAL, 2),
+		TOOL_DRAW_APPEND_TERRAIN("Draw terrain (D)", , 1),
+		TOOL_DRAW_ERASE_TERRAIN("Add/Remove area to/from terrain (Shift+D)", , 1),
+		TOOL_DRAW_CORNER_ADD_TERRAIN("Add/Move terrain corners", , 1),
+		TOOL_DRAW_CORNER_REMOVE_TERRAIN("Remove terrain corners", , 1),
+		TOOL_DRAW_APPEND_ACTOR("Draw shape (T)", , TAB_VISUAL, 2),
+		TOOL_DRAW_ERASE_ACTOR("Add/Remove area to/from shape (Shift+T)", , TAB_VISUAL, 2),
+		TOOL_DRAW_CORNER_ADD_ACTOR("Add/Move shape corners (C)", , TAB_VISUAL, 2),
+		TOOL_DRAW_CORNER_REMOVE_ACTOR("Remove shape corners (Shift+C)", , TAB_VISUAL, 2),
+		TOOL_ENEMY_ADD("Add enemies (E)", , 1),
+		TOOL_PATH("Add paths for enemies (P)", , 1),
+		TOOL_TRIGGER_ACTIVATE("Set enemy activate triggers (T)", , 1),
+		TOOL_TRIGGER_DEACTIVATE("Set enemy deactivate triggers (Shift+T)", , 1),
+		TOOL_CENTER_SET("Set center", , TAB_VISUAL, 2),
+		TOOL_CENTER_RESET("Reset center", , TAB_VISUAL, 2),
 
 		// File
 		FILE_NEW_CAMPAIGN("New campaign"),
@@ -270,71 +270,69 @@ public class Messages {
 		FILE_NEW_ENEMY("New enemy"),
 		FILE_NEW_BULLET("New bullet"),
 		FILE_NEW_SHIP("New ship"),
-		FILE_DUPLICATE_LEVEL("Create a copy of this level"),
-		FILE_DUPLICATE_ENEMY("Create a copy of this enemy"),
-		FILE_DUPLICATE_BULLET("Create a copy of this bullet"),
-		FILE_DUPLICATE_SHIP("Create a copy of this ship"),
+		FILE_DUPLICATE_LEVEL("Create a copy of this level", ),
+		FILE_DUPLICATE_ENEMY("Create a copy of this enemy", ),
+		FILE_DUPLICATE_BULLET("Create a copy of this bullet", ),
+		FILE_DUPLICATE_SHIP("Create a copy of this ship", ),
 		FILE_SAVE("Save"),
 		FILE_OPEN("Open"),
 		FILE_PUBLISH_CAMPAIGN("Publish campaign with levels"),
-		FILE_PUBLISH_LEVEL("Publish level with actors"),
-		FILE_PUBLISH_ENEMY("Publish this enemy"),
-		FILE_PUBLISH_BULLET("Publish this bullet"),
+		FILE_PUBLISH_LEVEL("Publish level with actors", ),
+		FILE_PUBLISH_ENEMY("Publish this enemy", ),
+		FILE_PUBLISH_BULLET("Publish this bullet", ),
 		FILE_INFO_CAMPAIGN("Campaign settings"),
-		FILE_INFO_LEVEL("Level settings", "https://www.youtube.com/watch?v=MiQCTPsKMS0"),
+		FILE_INFO_LEVEL("Level settings", ),
 		FILE_INFO_ENEMY("Enemy settings"),
 		FILE_INFO_BULLET("Bullet settings"),
 		FILE_INFO_SHIP("Ship name"),
 
 		// Editors
 		EDITOR_CAMPAIGN("Campaign editor"),
-		EDITOR_LEVEL("Level editor", "https://www.youtube.com/watch?v=MiQCTPsKMS0"),
-		EDITOR_ENEMY("Enemy editor", "https://www.youtube.com/watch?v=MKDrEu0leYA"),
-		EDITOR_BULLET("Bullet editor", "https://www.youtube.com/watch?v=guOTjFNKRJU"),
+		EDITOR_LEVEL("Level editor", ),
+		EDITOR_ENEMY("Enemy editor", ),
+		EDITOR_BULLET("Bullet editor", ),
 		EDITOR_SHIP("Ship editor"),
 
 		// Top bar middle actions
-		ACTION_UNDO("Undo", "https://www.youtube.com/watch?v=jhUNPagEd7Y"),
-		ACTION_REDO("Redo", "https://www.youtube.com/watch?v=jhUNPagEd7Y"),
+		ACTION_UNDO("Undo", ),
+		ACTION_REDO("Redo", ),
 		ACTION_BUG_REPORT("Bug Report"),
-		ACTION_GRID_TOGGLE("Toggle grid", "https://www.youtube.com/watch?v=jhUNPagEd7Y"),
-		ACTION_GRID_ABOVE("Toggle grid above actors", "https://www.youtube.com/watch?v=jhUNPagEd7Y"),
-		ACTION_SHOW_BACKGROUND("Toggle background", "https://www.youtube.com/watch?v=jhUNPagEd7Y"),
-		ACTION_ENEMY_SPAWN("Toggle enemy outline spawns", "https://www.youtube.com/watch?v=dM9aY8YqbAc"),
-		ACTION_PLAY_FROM_HERE("Test play level from current position"),
-		ACTION_PLAY_FROM_START("Test play level"),
+		ACTION_GRID_TOGGLE("Toggle grid", ),
+		ACTION_GRID_ABOVE("Toggle grid above actors", ),
+		ACTION_PLAY_FROM_HERE("Test play level from current position", ),
+		ACTION_PLAY_FROM_START("Test play level", ),
 
 		// Path tab (level editor)
-		PATH_LOOP("Enemies loop", "https://www.youtube.com/watch?v=DWeUECW8o2w", TAB_PATH),
-		PATH_ONCE("Enemies follow path once", "https://www.youtube.com/watch?v=DWeUECW8o2w", TAB_PATH),
-		PATH_BACK_AND_FORTH("Enemies goes back and forth", "https://www.youtube.com/watch?v=DWeUECW8o2w", TAB_PATH),
+		PATH_LOOP("Enemies loop", , TAB_PATH),
+		PATH_ONCE("Enemies follow path once", , TAB_PATH),
+		PATH_BACK_AND_FORTH("Enemies goes back and forth", , TAB_PATH),
 
 		// Enemy add tab (level editor)
 		ENEMY_ADD_TO_LIST("Add enemy to list"),
 
 		// Enemy settings tab (level editor)
-		ENEMY_SPAWN_DELAY("Spawn time between copies", "https://www.youtube.com/watch?v=tzHQkAxrx94", TAB_ENEMY),
-		ENEMY_ACTIVATION_DELAY("Spawn delay", "https://www.youtube.com/watch?v=tzHQkAxrx94", TAB_ENEMY),
-		ENEMY_DEACTIVATION_DELAY("Despawn delay", "https://www.youtube.com/watch?v=tzHQkAxrx94", TAB_ENEMY),
+		ENEMY_SPAWN_DELAY("Spawn time between copies", , TAB_ENEMY),
+		ENEMY_ACTIVATION_DELAY("Spawn delay", , TAB_ENEMY),
+		ENEMY_DEACTIVATION_DELAY("Despawn delay", , TAB_ENEMY),
 
 		// Enemy movement tab (enemy editor)
-		MOVEMENT_PATH("Path movement", "https://www.youtube.com/watch?v=whL_LQK64PA", TAB_MOVEMENT, 2),
-		MOVEMENT_STATIONARY("Stationary enemy", "https://www.youtube.com/watch?v=whL_LQK64PA", TAB_MOVEMENT, 2),
-		MOVEMENT_AI("AI movement", "https://www.youtube.com/watch?v=whL_LQK64PA", TAB_MOVEMENT, 2),
-		MOVEMENT_AI_RANDOM_COOLDOWN("Cooldown for new movement direction", "https://www.youtube.com/watch?v=whL_LQK64PA", MOVEMENT_AI),
+		MOVEMENT_PATH("Path movement", , TAB_MOVEMENT, 2),
+		MOVEMENT_STATIONARY("Stationary enemy", , TAB_MOVEMENT, 2),
+		MOVEMENT_AI("AI movement", , TAB_MOVEMENT, 2),
+		MOVEMENT_AI_RANDOM_COOLDOWN("Cooldown for new movement direction", , MOVEMENT_AI),
 
 		// Enemy weapon tab (enemy editor)
-		AIM_ON_PLAYER("Aim on player", "https://www.youtube.com/watch?v=SxzoVL5YTHc", TAB_WEAPON),
-		AIM_IN_FRONT_OF_PLAYER("Aim in front of player", "https://www.youtube.com/watch?v=SxzoVL5YTHc", TAB_WEAPON),
-		AIM_MOVEMENT_DIRECTION("Shoot in enemy movement direction", "https://www.youtube.com/watch?v=SxzoVL5YTHc", TAB_WEAPON),
-		AIM_DIRECTION("Shoot in a specific direction", "https://www.youtube.com/watch?v=SxzoVL5YTHc", TAB_WEAPON),
-		AIM_ROTATE("Shoot in a rotating manner", "https://www.youtube.com/watch?v=SxzoVL5YTHc", TAB_WEAPON),
+		AIM_ON_PLAYER("Aim on player", , TAB_WEAPON),
+		AIM_IN_FRONT_OF_PLAYER("Aim in front of player",, TAB_WEAPON),
+		AIM_MOVEMENT_DIRECTION("Shoot in enemy movement direction", , TAB_WEAPON),
+		AIM_DIRECTION("Shoot in a specific direction", , TAB_WEAPON),
+		AIM_ROTATE("Shoot in a rotating manner", , TAB_WEAPON),
 
 		// Visual tab (actor editor)
-		VISUAL_CUSTOM("Draw custom shape", "https://www.youtube.com/watch?v=q9BmR6E5JvM", TAB_VISUAL),
+		VISUAL_CUSTOM("Draw custom shape", , TAB_VISUAL),
 
 		// Collision tab (enemy editor)
-		COLLISION_DESTROY("Destroy enemy if it collides with player", "https://www.youtube.com/watch?v=ZbKIrmIbrjk", TAB_COLLISION),
+		COLLISION_DESTROY("Destroy enemy if it collides with player", , TAB_COLLISION),
 
 
 		;
@@ -350,36 +348,36 @@ public class Messages {
 		/**
 		 * Constructs a temporary tooltip
 		 * @param text tooltip text to display
-		 * @param youtubeLink link to YouTube tutorial
+		 * @param youtubeTime time in the YouTube tutorial
 		 */
-		private EditorTooltips(String text, String youtubeLink) {
+		private EditorTooltips(String text, String youtubeTime) {
 			mText = text;
-			mYoutubeLink = youtubeLink;
+			mYoutubeTime = youtubeTime;
 		}
 
 		/**
 		 * Constructs a temporary tooltip
 		 * @param text tooltip text to display
-		 * @param youtubeLink link to YouTube tutorial
+		 * @param youtubeTime time in the YouTube tutorial
 		 * @param parent parent tooltip. Set to null if this is a root tooltip
 		 */
-		private EditorTooltips(String text, String youtubeLink, ITooltip parent) {
+		private EditorTooltips(String text, String youtubeTime, ITooltip parent) {
 			mText = text;
-			mYoutubeLink = youtubeLink;
+			mYoutubeTime = youtubeTime;
 			mParent = parent;
 		}
 
 		/**
 		 * Constructs a permanent tooltip
 		 * @param text tooltip text to display
-		 * @param youtubeLink link to YouTube tutorial
+		 * @param youtubeTime time in the YouTube tutorial
 		 * @param parent parent tooltip. Set to null if this is a root tooltip
 		 * @param permanentLevel set to the level of priority the permanent should have.
 		 *        Set to null if you don't want this tooltip to be a permanent
 		 */
-		private EditorTooltips(String text, String youtubeLink, ITooltip parent, Integer permanentLevel) {
+		private EditorTooltips(String text, String youtubeTime, ITooltip parent, Integer permanentLevel) {
 			mText = text;
-			mYoutubeLink = youtubeLink;
+			mYoutubeTime = youtubeTime;
 			mPermanentLevel = permanentLevel;
 			mParent = parent;
 		}
@@ -387,20 +385,20 @@ public class Messages {
 		/**
 		 * Constructs a permanent tooltip
 		 * @param text tooltip text to display
-		 * @param youtubeLink link to YouTube tutorial
+		 * @param youtubeTime time in the YouTube tutorial
 		 * @param permanentLevel set to the level of priority the permanent should have.
 		 *        Set to null if you don't want this tooltip to be a permanent
 		 */
-		private EditorTooltips(String text, String youtubeLink, Integer permanentLevel) {
+		private EditorTooltips(String text, String youtubeTime, Integer permanentLevel) {
 			mText = text;
-			mYoutubeLink = youtubeLink;
+			mYoutubeTime = youtubeTime;
 			mPermanentLevel = permanentLevel;
 		}
 
 		/**
 		 * Constructs a temporary tooltip
 		 * @param text tooltip text to display
-		 * @param youtubeLink link to YouTube tutorial, may be null
+		 * @param youtubeTime time in the YouTube tutorial
 		 * @param parent parent tooltip. Set to null if this is a root tooltip
 		 * @param permanentLevel set to the level of priority the permanent should have.
 		 *        Set to null if you don't want this tooltip to be a permanent
@@ -412,13 +410,13 @@ public class Messages {
 		 *        hidden. If false the tooltip will be shown even though the actor is
 		 *        hidden.
 		 */
-		private EditorTooltips(String text, String youtubeLink, ITooltip parent, Integer permanentLevel, String hotkey, boolean youtubeOnly,
+		private EditorTooltips(String text, String youtubeTime, ITooltip parent, Integer permanentLevel, String hotkey, boolean youtubeOnly,
 				boolean hideWhenHidden) {
 			mText = text;
 			mPermanentLevel = permanentLevel;
 			mParent = parent;
 			mHotkey = hotkey;
-			mYoutubeLink = youtubeLink;
+			mYoutubeTime = youtubeTime;
 			mYoutubeOnly = youtubeOnly;
 			mHideWhenHidden = hideWhenHidden;
 		}
@@ -435,7 +433,11 @@ public class Messages {
 
 		@Override
 		public String getYoutubeLink() {
-			return mYoutubeLink;
+			if (mYoutubeTime != null) {
+			return YOUTUBE_URL + "#t=" + mYoutubeTime;
+			} else {
+				return null;
+			}
 		}
 
 		@Override
@@ -455,7 +457,7 @@ public class Messages {
 
 		@Override
 		public boolean hasYoutubeLink() {
-			return mYoutubeLink != null;
+			return mYoutubeTime != null;
 		}
 
 		@Override
@@ -477,16 +479,20 @@ public class Messages {
 		private String mText;
 		/** Hotkey for tooltip */
 		private String mHotkey = null;
-		/** YouTube link */
-		private String mYoutubeLink = null;
 		/** If the tooltip is YouTube-only */
 		private boolean mYoutubeOnly = false;
+		/** YouTube time */
+		private String mYoutubeTime = null;
 		/** Parent tooltip */
 		private ITooltip mParent;
 		/** Level of the tooltip */
 		private Integer mPermanentLevel = null;
 		/** Should hide when hidden */
 		private boolean mHideWhenHidden = true;
+
+
+		/** YouTube video */
+		private static final String YOUTUBE_URL = "https://youtu.be/KRPMoLZ2ZN8";
 	}
 
 	/**
