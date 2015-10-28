4b737bdfddc8f9756ae76fb144d241b48e261af5
Senth <senth.wallace@gmail.com>
2015-10-28 17:05:24 +0100

    Initial backup cleanup
    Refs #580 (0.9)
     * Created initial backup cleanup class and have an idea how to progress.

---
+++ game/src/Voider/server/src/main/java/com/spiddekauga/voider/servlets/cron/BackupCleanup.java
@@ -0,0 +1,16 @@
+package com.spiddekauga.voider.servlets.cron;
+
+import com.spiddekauga.voider.server.util.VoiderController;
+
+/**
+ * Cleanup old backup
+ * @author Matteus Magnusson <matteus.magnusson@spiddekauga.com>
+ */
+@SuppressWarnings("serial")
+public class BackupCleanup extends VoiderController {
+
+	@Override
+	protected void onRequest() {
+
+	}
+}
