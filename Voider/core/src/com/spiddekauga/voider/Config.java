a8015ff50c71db459cc8bca2811587d91a95cf0d
senth <senth.wallace@gmail.com>
2016-11-01 16:07:02 +0100

    Fix deploy game and appengine

--- Voider/core/src/com/spiddekauga/voider/Config.java
+++ Voider/core/src/com/spiddekauga/voider/Config.java
@@ -1,11 +1,5 @@
 package com.spiddekauga.voider;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-
-import javax.crypto.spec.SecretKeySpec;
-
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
@@ -18,13 +12,32 @@ import com.spiddekauga.voider.resources.IResourceEditorRender;
 import com.spiddekauga.voider.resources.IResourcePosition;
 import com.spiddekauga.voider.resources.IResourceRenderShape;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.crypto.spec.SecretKeySpec;
+
 /**
  * Game configuration
  * @author Matteus Magnusson <matteus.magnusson@spiddekauga.com>
  */
 public class Config {
 	/**
-	 * Dispose all objects that have been initialized
+	 * Revision of the game, as in code. This allows files of older revisions to be loaded into new
+	 * revisions
+	 */
+	public final static int REVISION = 3;
+
+/**
+ * Private constructor so that no instance can be created
+ */
+private Config() {
+	// Does nothing
+}
+
+/**
+ * Dispose all objects that have been initialized
 	 */
 	public static void dispose() {
 		// Does nothing
@@ -34,6 +47,23 @@ public class Config {
 	 * Actors
 	 */
 	public static class Actor {
+		/** Border width of all actors */
+		public final static float BORDER_WIDTH = 0.5f;
+		/** Bounding box color */
+		public final static Color BOUNDING_BOX_COLOR = Color.CYAN;
+		/** Outline color */
+		public final static Color OUTLINE_COLOR = Color.ORANGE;
+		/** Closing outline color (color from corner.end -> corner.begin) */
+		public final static Color OUTLINE_CLOSE_COLOR = Color.PINK;
+		/** Default name of definitions */
+		public final static String NAME_DEFAULT = "(Unnamed)";
+		/** Minimum name length */
+		public final static int NAME_LENGTH_MIN = 3;
+		/** Texture size when saving */
+		public final static int SAVE_TEXTURE_SIZE = 128;
+		/** Texture size of layered images on top of the actor */
+		public final static int SAVE_IMAGE_ON_ACTOR_SIZE = SAVE_TEXTURE_SIZE / 4;
+
 		/**
 		 * Bullet
 		 */
@@ -85,23 +115,6 @@ public class Config {
 			/** Maximum player heath */
 			public final static float HEALTH_MAX = 100;
 		}
-
-		/** Border width of all actors */
-		public final static float BORDER_WIDTH = 0.5f;
-		/** Bounding box color */
-		public final static Color BOUNDING_BOX_COLOR = Color.CYAN;
-		/** Outline color */
-		public final static Color OUTLINE_COLOR = Color.ORANGE;
-		/** Closing outline color (color from corner.end -> corner.begin) */
-		public final static Color OUTLINE_CLOSE_COLOR = Color.PINK;
-		/** Default name of definitions */
-		public final static String NAME_DEFAULT = "(Unnamed)";
-		/** Minimum name length */
-		public final static int NAME_LENGTH_MIN = 3;
-		/** Texture size when saving */
-		public final static int SAVE_TEXTURE_SIZE = 128;
-		/** Texture size of layered images on top of the actor */
-		public final static int SAVE_IMAGE_ON_ACTOR_SIZE = SAVE_TEXTURE_SIZE / 4;
 	}
 
 	/**
@@ -109,8 +122,7 @@ public class Config {
 	 */
 	public static class Cache {
 		/**
-		 * How long time resource when browsing should be available after initial fetch,
-		 * in seconds
+		 * How long time resource when browsing should be available after initial fetch, in seconds
 		 */
 		public static final int RESOURCE_BROWSE_TIME = 300;
 		/** Highscore cache, in seconds */
@@ -139,6 +151,22 @@ public class Config {
 	 * Encryption
 	 */
 	public static class Crypto {
+		/** Salt for file key */
+		private static final byte[] FILE_KEY_BYTES = {15, 35, 68, 86, 57, 2, 99, 105, 127, -38, -100, -35, 35, 48, 68, -79, 95, -22, 0, 15, 0, 0, 98, 15, 27, 35};
+		/** Salt for file key */
+		private static final byte[] PASSWORD_KEY_BYTES = {11, 120, 8, 86, 5, 22, 9, 15, -88, 38, 100, -35, 35, 35, -6, 79, 95, 22, 22, 2, 15, 65, 8, -15, -27, -35};
+		/** The actual file key */
+		private static SecretKeySpec mFileKey = null;
+
+		// @formatter:off
+		/** The actual password key */
+		private static SecretKeySpec mPasswordKey = null;
+
+		static {
+			init();
+		}
+		// @formatter:on
+
 		/**
 		 * Initialization of the config class
 		 */
@@ -180,50 +208,27 @@ public class Config {
 		public static SecretKeySpec getPasswordKey() {
 			return mPasswordKey;
 		}
-
-		// @formatter:off
-		/** Salt for file key */
-		private static final byte[] FILE_KEY_BYTES = { 15, 35, 68, 86, 57, 2, 99, 105, 127, -38, -100, -35, 35, 48, 68, -79, 95, -22, 0, 15, 0, 0, 98, 15, 27, 35 };
-		/** Salt for file key */
-		private static final byte[] PASSWORD_KEY_BYTES = { 11, 120, 8, 86, 5, 22, 9, 15, -88, 38, 100, -35, 35, 35, -6, 79, 95, 22, 22, 2, 15, 65, 8, -15, -27, -35 };
-		// @formatter:on
-		/** The actual file key */
-		private static SecretKeySpec mFileKey = null;
-		/** The actual password key */
-		private static SecretKeySpec mPasswordKey = null;
-
-		static {
-			init();
-		}
 	}
 
 	/**
 	 * Debug options
 	 */
 	public static class Debug {
-		/**
-		 * Control over debug messages
-		 */
-		public static class Messages {
-			/** If loading/unloading debug messages should be turned on/off */
-			public static final boolean LOAD_UNLOAD = false;
-		}
-
 		/** Build level */
 		public static final Builds BUILD = Builds.BETA;
-		/** Set to true in JUNIT tests */
-		public static boolean JUNIT_TEST = false;
 		/** Logging verbosity */
 		public static final int LOG_VERBOSITY = isBuildOrAbove(Builds.RELEASE) ? Application.LOG_ERROR : Application.LOG_DEBUG;
+		/** Skip loading text */
+		public static final boolean SKIP_LOADING_TIME = isBuildOrBelow(Builds.NIGHTLY_RELEASE) ? true : false;
+		/** Beta key length */
+		public static final int REGISTER_KEY_LENGTH = 22;
+		/** Set to true in JUNIT tests */
+		public static boolean JUNIT_TEST = false;
 		/**
 		 * If debugging tests shall be activate. This causes extra runtime, but checks so
 		 * that none of the checks are broken.
 		 */
 		public static boolean DEBUG_TESTS = isBuildOrBelow(Builds.NIGHTLY_DEV);
-		/** Skip loading text */
-		public static final boolean SKIP_LOADING_TIME = isBuildOrBelow(Builds.NIGHTLY_RELEASE) ? true : false;
-		/** Beta key length */
-		public static final int REGISTER_KEY_LENGTH = 22;
 
 		/**
 		 * Call this method to throw a deprecated error message. If the build is in
@@ -233,6 +238,14 @@ public class Config {
 			if (isBuildOrBelow(Builds.DEV_SERVER)) {
 				throw new DeprecatedException();
 			}
+		}
+
+		/**
+		 * @param build the build to check if we're at or below
+		 * @return true if the current build is equal to or below (development) than the specified build
+		 */
+		public static boolean isBuildOrBelow(Builds build) {
+			return BUILD.ordinal() <= build.ordinal();
 		}
 
 		/**
@@ -247,25 +260,12 @@ public class Config {
 		}
 
 		/**
-		 * Debug exception
+		 * @param build the build to check if we're at or above
+		 * @return true if the current build is equal to or above (later) than the
+		 *         specified build
 		 */
-		public static class DebugException extends RuntimeException {
-			/**
-			 * @param message the message why this exception is thrown
-			 */
-			public DebugException(String message) {
-				super(message);
-			}
-
-			private static final long serialVersionUID = -4071174543732515552L;
-
-		}
-
-		/**
-		 * Deprecated exception
-		 */
-		public static class DeprecatedException extends RuntimeException {
-			private static final long serialVersionUID = -2515360644807136974L;
+		public static boolean isBuildOrAbove(Builds build) {
+			return BUILD.ordinal() >= build.ordinal();
 		}
 
 		/**
@@ -288,21 +288,33 @@ public class Config {
 		}
 
 		/**
-		 * @param build the build to check if we're at or above
-		 * @return true if the current build is equal to or above (later) than the
-		 *         specified build
+		 * Control over debug messages
 		 */
-		public static boolean isBuildOrAbove(Builds build) {
-			return BUILD.ordinal() >= build.ordinal();
+		public static class Messages {
+			/** If loading/unloading debug messages should be turned on/off */
+			public static final boolean LOAD_UNLOAD = false;
 		}
 
 		/**
-		 * @param build the build to check if we're at or below
-		 * @return true if the current build is equal to or below (development) than the
-		 *         specified build
+		 * Debug exception
 		 */
-		public static boolean isBuildOrBelow(Builds build) {
-			return BUILD.ordinal() <= build.ordinal();
+		public static class DebugException extends RuntimeException {
+			private static final long serialVersionUID = -4071174543732515552L;
+
+			/**
+			 * @param message the message why this exception is thrown
+			 */
+			public DebugException(String message) {
+				super(message);
+			}
+
+		}
+
+		/**
+		 * Deprecated exception
+		 */
+		public static class DeprecatedException extends RuntimeException {
+			private static final long serialVersionUID = -2515360644807136974L;
 		}
 	}
 
@@ -310,6 +322,38 @@ public class Config {
 	 * Editor options
 	 */
 	public static class Editor {
+
+		/**
+		 * When to only show 10ths of coordinates. True when pixels per world coordinate is below this
+		 * value
+		 */
+		public final static int GRID_SHOW_ONLY_MILESTONE_PIXELS_PER_WORLD = 4;
+		/** Regular step size of grid */
+		public final static int GRID_STEP_SIZE = 2;
+		/** Milestone step size. Should be a multiplier of GRID_STEP_SIZE! */
+		public final static int GRID_MILESTONE_STEP = GRID_STEP_SIZE * 5;
+		/** Corner pick color */
+		public final static Color CORNER_COLOR = new Color(0.75f, 0, 0, 1);
+		/** Center offset pick color */
+		public final static Color CENTER_OFFSET_COLOR = new Color(0, 0.75f, 0, 1);
+		/** Default pick size */
+		public final static float PICK_SIZE_DEFAULT = 0.0001f;
+		/** Path pick size */
+		public final static float PICK_PATH_SIZE = 0.5f;
+		/** Trigger pick size */
+		public final static float PICK_TRIGGER_SIZE = PICK_PATH_SIZE;
+		/** Radius of all picking circles */
+		public final static float PICKING_CIRCLE_RADIUS_EDITOR = 1;
+		/** Radius of level editor picking circles */
+		public final static float PICKING_CIRCLE_RADIUS_LEVEL_EDITOR = 2;
+		/** Brush draw add color */
+		public final static Color BRUSH_ADD_COLOR = Color.GREEN;
+		/** Brush erase color */
+		public final static Color BRUSH_ERASE_COLOR = Color.RED;
+		/** Selection brush color */
+		public final static Color BRUSH_SELECTION_COLOR = Color.GRAY;
+		/** Zoom in amount when clicking on the icon, this value is multiplied */
+		public final static float ZOOM_AMOUNT = 0.8f;
 
 		/**
 		 * Level editor
@@ -413,40 +457,6 @@ public class Config {
 			/** Step size of bullet damage */
 			public final static float DAMAGE_STEP_SIZE = 1;
 		}
-
-
-		/**
-		 * When to only show 10ths of coordinates. True when pixels per world coordinate
-		 * is below this value
-		 */
-		public final static int GRID_SHOW_ONLY_MILESTONE_PIXELS_PER_WORLD = 4;
-		/** Regular step size of grid */
-		public final static int GRID_STEP_SIZE = 2;
-		/** Milestone step size. Should be a multiplier of GRID_STEP_SIZE! */
-		public final static int GRID_MILESTONE_STEP = GRID_STEP_SIZE * 5;
-
-		/** Corner pick color */
-		public final static Color CORNER_COLOR = new Color(0.75f, 0, 0, 1);
-		/** Center offset pick color */
-		public final static Color CENTER_OFFSET_COLOR = new Color(0, 0.75f, 0, 1);
-		/** Default pick size */
-		public final static float PICK_SIZE_DEFAULT = 0.0001f;
-		/** Path pick size */
-		public final static float PICK_PATH_SIZE = 0.5f;
-		/** Trigger pick size */
-		public final static float PICK_TRIGGER_SIZE = PICK_PATH_SIZE;
-		/** Radius of all picking circles */
-		public final static float PICKING_CIRCLE_RADIUS_EDITOR = 1;
-		/** Radius of level editor picking circles */
-		public final static float PICKING_CIRCLE_RADIUS_LEVEL_EDITOR = 2;
-		/** Brush draw add color */
-		public final static Color BRUSH_ADD_COLOR = Color.GREEN;
-		/** Brush erase color */
-		public final static Color BRUSH_ERASE_COLOR = Color.RED;
-		/** Selection brush color */
-		public final static Color BRUSH_SELECTION_COLOR = Color.GRAY;
-		/** Zoom in amount when clicking on the icon, this value is multiplied */
-		public final static float ZOOM_AMOUNT = 0.8f;
 	}
 
 	/**
@@ -475,35 +485,6 @@ public class Config {
 		private static String mUserStorage;
 		/** User preferences prefix */
 		private static String mUserPreferencesPrefix;
-
-		/**
-		 * @return user preferences prefix
-		 */
-		public static String getUserPreferencesPrefix() {
-			return mUserPreferencesPrefix;
-		}
-
-		/**
-		 * @return user storage path
-		 */
-		public static String getUserStorage() {
-			return mUserStorage;
-		}
-
-		/**
-		 * Set the user paths
-		 * @param username username of the user to get the path
-		 */
-		public static void setUserPaths(String username) {
-			mUserStorage = STORAGE + username + "/";
-			mUserPreferencesPrefix = PREFERENCE_PREFIX + "." + username + ".";
-
-			// Create folder if it doesn't exist
-			FileHandle folder = Gdx.files.external(mUserStorage);
-			if (!folder.exists()) {
-				folder.mkdirs();
-			}
-		}
 
 		static {
 			String initialPrefix = "Voider";
@@ -541,6 +522,35 @@ public class Config {
 			}
 			STORAGE = PREFERENCE_PREFIX + "/";
 		}
+
+		/**
+		 * @return user preferences prefix
+		 */
+		public static String getUserPreferencesPrefix() {
+			return mUserPreferencesPrefix;
+		}
+
+		/**
+		 * @return user storage path
+		 */
+		public static String getUserStorage() {
+			return mUserStorage;
+		}
+
+		/**
+		 * Set the user paths
+		 * @param username username of the user to get the path
+		 */
+		public static void setUserPaths(String username) {
+			mUserStorage = STORAGE + username + "/";
+			mUserPreferencesPrefix = PREFERENCE_PREFIX + "." + username + ".";
+
+			// Create folder if it doesn't exist
+			FileHandle folder = Gdx.files.external(mUserStorage);
+			if (!folder.exists()) {
+				folder.mkdirs();
+			}
+		}
 	}
 
 	/**
@@ -570,14 +580,10 @@ public class Config {
 		public final static boolean USE_DEBUG_RENDERER = false;
 		/** Renders regular graphics */
 		public final static boolean USE_RELEASE_RENDERER = true;
-		/** Epsilon for box 2d */
-		private final static float EPSILON = 1.19209289550781250000e-7F;
 		/** Minimum length between two corners in a polygon, squared */
 		public final static float EDGE_LENGTH_MIN_SQUARED = 0.0025f;
 		/** Minimum length between two corners in a polygon */
 		public final static float EDGE_LENGTH_MIN = (float) Math.sqrt(EDGE_LENGTH_MIN_SQUARED);
-		/** Minimum area of a polygon shape */
-		public final static float POLYGON_AREA_MIN = EPSILON * 10000f;
 		/** Default width of the graphics, also minimum */
 		public final static int WIDTH_DEFAULT = 800;
 		/** Default height of the graphics, also minimum */
@@ -603,6 +609,10 @@ public class Config {
 		public final static float DEPTH_STEP_SIZE = 0.001f;
 		/** Widescreen ratio */
 		public final static float WIDESCREEN_RATIO = 1.6f / 0.9f;
+		/** Epsilon for box 2d */
+		private final static float EPSILON = 1.19209289550781250000e-7F;
+		/** Minimum area of a polygon shape */
+		public final static float POLYGON_AREA_MIN = EPSILON * 10000f;
 
 		/**
 		 * Z-value for rendering objects. The further up the enumeration is located the
@@ -638,29 +648,20 @@ public class Config {
 			/** Grid below */
 			GRID_BELOW,
 			/** Background */
-			BACKGROUND,
+			BACKGROUND,;
 
-			;
-
-			/**
-			 * @return the render order determining in what order to render the objects
-			 */
-			public int getOrder() {
-				return ordinal();
-			}
+			/** Minor step size */
+			private final static float MINOR_STEP_SIZE = 0.001f;
+			/** Current global offset */
+			private static float mGlobalZValueOffset = 0;
+			/** The z-value */
+			private float mZValue = 0;
 
 			/**
 			 * Default constructor that sets the z-value
 			 */
 			private RenderOrders() {
 				mZValue = -ordinal();
-			}
-
-			/**
-			 * @return z-value of the object to render
-			 */
-			public float getZValue() {
-				return mZValue;
 			}
 
 			/**
@@ -679,6 +680,13 @@ public class Config {
 				}
 
 				shapeRenderer.translate(0, 0, -zValue);
+			}
+
+			/**
+			 * @return z-value of the object to render
+			 */
+			public float getZValue() {
+				return mZValue;
 			}
 
 			/**
@@ -777,12 +785,12 @@ public class Config {
 				mGlobalZValueOffset = 0;
 			}
 
-			/** The z-value */
-			private float mZValue = 0;
-			/** Current global offset */
-			private static float mGlobalZValueOffset = 0;
-			/** Minor step size */
-			private final static float MINOR_STEP_SIZE = 0.001f;
+			/**
+			 * @return the render order determining in what order to render the objects
+			 */
+			public int getOrder() {
+				return ordinal();
+			}
 		}
 	}
 
@@ -838,7 +846,6 @@ public class Config {
 		/** Screenshot texture height */
 		public final static int SAVE_TEXTURE_HEIGHT = (int) (SAVE_TEXTURE_WIDTH / SAVE_TEXTURE_RATIO);
 	}
-
 
 	/**
 	 * Network
@@ -901,18 +908,5 @@ public class Config {
 		public static final int PASSWORD_LENGTH_MIN = 5;
 		/** Minimum username lengeth */
 		public static final int NAME_LENGTH_MIN = 3;
-	}
-
-	/**
-	 * Revision of the game, as in code. This allows files of older revisions to be loaded
-	 * into new revisions
-	 */
-	public final static int REVISION = 3;
-
-	/**
-	 * Private constructor so that no instance can be created
-	 */
-	private Config() {
-		// Does nothing
 	}
 }
